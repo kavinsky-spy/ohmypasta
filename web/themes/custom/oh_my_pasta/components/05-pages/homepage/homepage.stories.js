d342e40ffb1887b84d77442164df9fbb4f083fd9
Julio Retkwa <julio@zoocha.com>
2024-11-21 19:18:28 -0300

    chore: add homepage

--- web/themes/custom/oh_my_pasta/components/05-pages/homepage/homepage.stories.js
+++ web/themes/custom/oh_my_pasta/components/05-pages/homepage/homepage.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import template from './homepage.twig';
+import '../../03-organisms/tabs-listing/tabs-listing';
 
 export default {
   title: 'Pages/Homepage',
@@ -18,4 +19,153 @@ const Template = (args) => (
 export const Homepage = Template.bind({});
 Homepage.args = {
   title: 'test',
+  tabs_list: [
+    {
+      title_tab: 'Top Cities',
+      list_titles: [
+        {
+          title_list: 'New York',
+        },
+        {
+          title_list: 'Brooklyn',
+        },
+        {
+          title_list: 'Atlanta',
+        },
+        {
+          title_list: 'Los Angeles',
+        },
+        {
+          title_list: 'San Diego',
+        },
+        {
+          title_list: 'Queens',
+        },
+        {
+          title_list: 'Toronto',
+        },
+        {
+          title_list: 'Las Vegas',
+        },
+        {
+          title_list: 'Vancouver',
+        },
+        {
+          title_list: 'Chicago',
+        },
+        {
+          title_list: 'San Francisco',
+        },
+        {
+          title_list: 'Miami',
+        },
+        {
+          title_list: 'Houston',
+        },
+        {
+          title_list: 'Seattle',
+        },
+        {
+          title_list: 'San Antonio',
+        },
+      ],
+    },
+    {
+      title_tab: 'Top Cuisines',
+      list_titles: [
+        {
+          title_list: 'Pizza',
+        },
+        {
+          title_list: 'Lunch',
+        },
+        {
+          title_list: 'Asian Food',
+        },
+        {
+          title_list: 'Chinese Food',
+        },
+        {
+          title_list: 'Seafood',
+        },
+        {
+          title_list: 'Italian Food',
+        },
+        {
+          title_list: 'Sushi',
+        },
+        {
+          title_list: 'Indian Food',
+        },
+        {
+          title_list: 'Vegan Food',
+        },
+        {
+          title_list: 'Cafe',
+        },
+        {
+          title_list: 'Dessert',
+        },
+        {
+          title_list: 'Sandwiches',
+        },
+        {
+          title_list: 'Burgers',
+        },
+        {
+          title_list: 'Restaurants',
+        },
+      ],
+    },
+    {
+      title_tab: 'Top Chains',
+      list_titles: [
+        {
+          title_list: "Raising Cane's",
+        },
+        {
+          title_list: 'Subway',
+        },
+        {
+          title_list: "Jersey Mike's",
+        },
+        {
+          title_list: 'Starbucks',
+        },
+        {
+          title_list: 'Buffalo Wild Wings',
+        },
+        {
+          title_list: 'KFC',
+        },
+        {
+          title_list: 'Chick-fil-A',
+        },
+        {
+          title_list: 'Chipotle',
+        },
+        {
+          title_list: 'The Cheesecake Facto',
+        },
+        {
+          title_list: 'Taco Bell',
+        },
+        {
+          title_list: 'Panera Bread',
+        },
+        {
+          title_list: 'Panda Express',
+        },
+        {
+          title_list: "McDonald's",
+        },
+        {
+          title_list: "Wendy's",
+        },
+        {
+          title_list: 'Burger King',
+        },
+      ],
+    },
+  ],
 };
